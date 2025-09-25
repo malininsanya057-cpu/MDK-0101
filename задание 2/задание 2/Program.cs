01a9c580675574c0751efd6eb65e3415ce0255b7
user <malininsanya057@gmail.com>
2025-09-25 18:18:18 +0300

    создали и заполнили массив случайными числами

---
+++ задание 2/задание 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_2
+{
+    public class Program
+    {
+        static void Main()
+        {
+           
+            Random random = new Random();  
+           
+            int[] numbers = new int[1500];
+            for (int i = 0; i < 1500; i++)  
+            {
+                numbers[i] = random.Next(1, 201);  
+            }
+
+
+        }
+    }
+}
