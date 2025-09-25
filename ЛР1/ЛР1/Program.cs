91d7efa8a7d2cb4082e7dc1c93e89f88cbe3b005
user <user@KOMPUTER>
2025-09-25 17:47:55 +0300

    Добавьте файлы проекта.

---
+++ ЛР1/ЛР1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
